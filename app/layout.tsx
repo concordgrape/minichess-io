b47b1524c7a043b1790c13b0f45ca36e10c0b39f
Sky Roth <55454027+concordgrape@users.noreply.github.com>
2026-07-16 21:41:19 -0400

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,12 @@ export const viewport: Viewport = {
 export const metadata: Metadata = {
   metadataBase: new URL("https://dailycheckmate.com"),
   alternates: { canonical: "./" },
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+  },
   title: {
     default: "Chess Puzzles",
     template: "%s | Chess Puzzles",
