f243f2487800299cc6dbc05eb5a0767649d992b4
Sky Roth <55454027+concordgrape@users.noreply.github.com>
2026-07-23 21:07:08 -0400

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,6 @@ export const metadata: Metadata = {
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
-    apple: [{ url: "/apple-icon.png", sizes: "180x180", type: "image/png" }],
   },
   title: {
     default: "Chess Puzzles",
